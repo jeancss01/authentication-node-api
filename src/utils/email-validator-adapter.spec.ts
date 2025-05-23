a14224439b3f454392b5b7ffc1f34b1c93d8e08e
Jean Santana <jean.santana@raizen.com>
2025-05-23 20:04:28 -0300

    feat: add email validator

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -3,14 +3,15 @@ import { EmailValidatorAdapter } from './email-validator'
 describe('EmailValidatorAdapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
-    // jest.spyOn(sut, 'isValid').mockReturnValueOnce(false)
-    const isValid = sut.isValid('invalid_email')
+    jest.spyOn(sut, 'isValid').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
 
-//   test('Should return true if validator returns true', () => {
-//     const sut = new EmailValidatorAdapter()
-//     // jest.spyOn(sut, 'isValid').mockReturnValueOnce(true)
-//     const isValid = sut.isValid('')
-//   })
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    jest.spyOn(sut, 'isValid').mockReturnValueOnce(true)
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
 })
