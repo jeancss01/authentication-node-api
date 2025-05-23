cd9edb4b1b30a2b1d12004ae9efa85f468b4acc7
Jean Santana <jean.santana@raizen.com>
2025-05-23 19:42:11 -0300

    feat: config test jest

---
+++ jest-integration-config.js
@@ -0,0 +1,4 @@
+import config from './jest.config.js'
+config.testMatch = ['**/*.test.js']
+module.exports = config
+// This file is used to run unit tests only
---
+++ jest-unit-config.js
@@ -0,0 +1,4 @@
+import config from './jest.config.js'
+config.testMatch = ['**/*.spec.js']
+module.exports = config
+// This file is used to run unit tests only
