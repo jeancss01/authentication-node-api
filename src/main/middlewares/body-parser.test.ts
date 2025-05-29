b4aa48504e38b6f8d8915ca234f8be307f710232
Jean Santana <jean.santana@raizen.com>
2025-05-28 21:50:53 -0300

    feat: ensure json as default content-type

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -4,11 +4,11 @@ import app from '../config/app'
 describe('Body Parser Middleware', () => {
   test('should parse JSON body', async () => {
     app.post('/test_body_parser', (req, res) => {
-      res.send(req.body.name)
+      res.send(req.body)
     })
     await request(app)
       .post('/test_body_parser')
       .send({ name: 'Jean Santana' })
-      .expect('Jean Santana')
+      .expect({ name: 'Jean Santana' })
   })
 })
