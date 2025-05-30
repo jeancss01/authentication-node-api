8f94f20236bc9e5cc763300d5023b8e0317ac32f
Jean Santana <jean.santana@raizen.com>
2025-05-29 22:09:04 -0300

    feat: ensure: LogMongoRepository

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,34 @@
+import { type Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helpers'
+import { LogMongoRepository } from './log'
+
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    const uri = process.env.MONGO_URL
+    if (!uri) {
+      throw new Error('MONGO_URL environment variable is not defined')
+    }
+    console.log('Mongo URI:', uri)
+    await MongoHelper.connect(uri)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = makeSut()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
