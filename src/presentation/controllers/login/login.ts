87874c2bb31f320ce32c8b461345a543a9103976
Jean Santana <jean.santana@raizen.com>
2025-05-30 18:18:21 -0300

    feat: ensure LoginController

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,9 +1,15 @@
-import { MissingParamError } from '../../errors'
+import { MissingParamError, ServerError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import type { HttpRequest, HttpResponse, Controller } from '../../protocols'
 
 export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError('email'))
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'))
+    }
+    if (!httpRequest.body.password) {
+      return badRequest(new MissingParamError('password'))
+    }
+    return badRequest(new ServerError('This is a placeholder for a successful response'))
   }
 }
