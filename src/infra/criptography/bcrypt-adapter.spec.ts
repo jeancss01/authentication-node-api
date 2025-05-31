bb244c3c6a5cb871c4f6910ec081ff09c38b4f37
Jean Santana <jean.santana@raizen.com>
2025-05-31 16:27:53 -0300

    feat: ensure BcryptAdapter return false when compare values

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -50,4 +50,28 @@ describe('BcryptAdapter', () => {
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(true)
   })
+  test('should return false when compare fails', async () => {
+    jest.mock('bcrypt', () => ({
+      async hash (): Promise<string> {
+        return 'hash'
+      },
+      async compare (): Promise<boolean> {
+        return false
+      }
+    }))
+    const sut = makeSut()
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => await Promise.resolve(false))
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
+  })
+
+  test('should throw if compare throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.compare('any_value', 'any_hash')
+    await expect(promise).rejects.toThrow()
+  })
 })
